cb190290bdb808b68d086b6612af2aa6cf85380a
Mohd Asad Naseem <87413907+AsadGuru@users.noreply.github.com>
2024-09-16 01:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	// create an object to count occurences of each character 
+
+	const charCount = {};
+
+	// first pass: count the occurences of each charater
+	for(let char of str) {
+		if (charCount[char]++) {
+		} else {
+			charCount[char] = 1
+		}
+	}
+
+	/ second seizee firl
+	for(let char of str) {
+		of(charCount[char] === 1) {
+			return char
+		}
+	}
+
+	// if no unique character is found, return null
+	return null
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
